317979b722f8423875fd4f5b2c9ad20d7ce8bfcf
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-03-21 11:20:12 +0530

    feat: implement salary submission endpoint

---
+++ backend/salary-service/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database.db_connection import get_db
+from database.models import SalarySubmission
+from .schemas import SalaryCreate
+
+app = FastAPI(title="Salary Submission Service")
+
+@app.post("/submit")
+def submit_salary(data: SalaryCreate, db: Session = Depends(get_db)):
+    new_submission = SalarySubmission(
+        role=data.role,
+        company=data.company,
+        country=data.country,
+        experience_level=data.experience_level,
+        amount=data.amount
+    )
+    db.add(new_submission)
+    db.commit()
+    db.refresh(new_submission)
+    return {"message": "Submission received", "id": str(new_submission.id)}
